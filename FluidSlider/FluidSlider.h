99dee2363a1b0623e65f7a4cedb6d86d6d086c8a
Nathan Ansel <nansel@nsparc.msstate.edu>
2017-10-12 15:21:02 -0500

    Initial Commit

---
+++ FluidSlider/FluidSlider.h
@@ -0,0 +1,19 @@
+//
+//  FluidSlider.h
+//  FluidSlider
+//
+//  Created by Nathan Ansel on 10/12/17.
+//  Copyright © 2017 Nathan Ansel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FluidSlider.
+FOUNDATION_EXPORT double FluidSliderVersionNumber;
+
+//! Project version string for FluidSlider.
+FOUNDATION_EXPORT const unsigned char FluidSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FluidSlider/PublicHeader.h>
+
+
